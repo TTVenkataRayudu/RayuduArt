cb55b9516759dbc484e99f1483be04126fa4e37b
Jeevitha Potukanuma <jpotukanuma@nationsbenefits.com>
2023-04-12 19:58:51 +0530

    Added images to paintings page

--- src/Components/PaintingsPage.tsx
+++ src/Components/PaintingsPage.tsx
@@ -1,7 +1,13 @@
+import { Gallery } from "react-grid-gallery"
+import { images } from "../images/images"
 
 export const PaintingsPage = () => {
     return (
-        <></>
+        <>
+            <Gallery
+                images={images}
+            />
+        </>
         )
 
 }
--- src/images/images.ts
+++ src/images/images.ts
@@ -1,8 +1,88 @@
-export const images = [
+import { Image } from "react-grid-gallery";
+import Image1 from "../images/Image1.jpg";
+import Image2 from "../images/Image2.jpg";
+import Image3 from "../images/Image3.jpg";
+import Image4 from "../images/Image4.jpg";
+import Image5 from "../images/Image5.jpg";
+import Image6 from "../images/Image6.jpg";
+import Image7 from "../images/Image7.jpg";
+import Image8 from "../images/Image8.jpg";
+import Image9 from "../images/Image9.jpg";
+import Image10 from "../images/Image10.jpg";
+import Image11 from "../images/Image11.png";
+import Image12 from "../images/Image12.jpg";
+
+export const images: Image[] = [
     {
-        src:"./images/Homepage.jpg",
-        width: "",
-        height: "",
+        src: Image1,
+        width: 3000,
+        height: 5000,
         caption:"image1"
+    },
+    {
+        src: Image2,
+        width: 160,
+        height: 210,
+        caption: "image2"
+    },
+    {
+        src: Image3,
+        width: 210,
+        height: 210,
+        caption: "image3"
+    },
+    {
+        src: Image4,
+        width: 140,
+        height: 160,
+        caption: "image4"
+    },
+    {
+        src: Image5,
+        width: 180,
+        height: 200,
+        caption: "image5"
+    },
+    {
+        src: Image6,
+        width: 200,
+        height: 210,
+        caption: "image6"
+    },
+    {
+        src: Image7,
+        width: 120,
+        height: 120,
+        caption: "image7"
+    },
+    {
+        src: Image8,
+        width: 130,
+        height: 140,
+        caption: "image8"
+    },
+    {
+        src: Image9,
+        width: 160,
+        height: 200,
+        caption: "image9"
+    },
+    {
+        src: Image10,
+        width: 189,
+        height: 210,
+        caption: "image10"
+    },
+    {
+        src: Image11,
+        width: 100,
+        height: 180,
+        caption: "image11"
+    },
+    {
+        src: Image12,
+        width: 150,
+        height: 210,
+        caption: "image12"
     }
 ]

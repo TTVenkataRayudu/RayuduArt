390cfff36b74ee7a29c6dd5b6f665eb8faca1d2f
Tirumala Venkatarayudu Thuthika <tthuthika@nationsbenefits.com>
2023-09-11 01:54:14 -0400

    styling for images

--- src/Components/SketchesPage.tsx
+++ src/Components/SketchesPage.tsx
@@ -1,25 +1,23 @@
-import { artimages } from "../images/images";
-import '../App.css'; 
+import React from 'react';
+import { artimages } from '../images/images';
+import '../App.css';
 export const SketchesPage = () => {
     return (
         <div className="productimage">
-            <div className="image">
-                {artimages.filter((x: any) => x.category === "pencilsketch").map(p => {
-                    return (
-                        <>
-                           <div className="image productimage" key={p.key}>
-                                <img src={p.ImageURL} />
-                            </div>
+            <div className="image-container">
+                {artimages
+                    .filter((x: any) => x.category === 'pencilsketch')
+                    .map((p) => (
+                        <div className="image" key={p.key}>
+                            <img src={p.ImageURL} alt={p.Name} />
                             <div className="description">
-                                <div >Name:{p.Name}</div>
-                                <p>Description:{p.Description}</p>
+                                <div>Name: {p.Name}</div>
+                                <p>Description: {p.Description}</p>
                             </div>
-                        </>
-                    )
-                })}  
+                        </div>
+                    ))}
             </div>
         </div>
-       
+    );
+};
 
-    )
-}

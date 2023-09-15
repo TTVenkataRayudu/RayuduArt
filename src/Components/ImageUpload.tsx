f355a9a716f3f41ce1ded679b35723098507da1b
Tirumala Venkatarayudu Thuthika <tthuthika@nationsbenefits.com>
2023-09-15 04:11:56 -0400

    Firebase image upload

--- src/Components/ImageUpload.tsx
+++ src/Components/ImageUpload.tsx
@@ -1,116 +1,22 @@
-import React, { ChangeEvent, FormEvent, useState } from 'react';
-import './ImageUpload.css'; // Import the CSS file
-
-const ImageUpload: React.FC = () => {
-    const [id, setId] = useState('');
-    const [name, setName] = useState('');
-    const [description, setDescription] = useState('');
-    const [type, setType] = useState('');
-    const [image, setImage] = useState<File | null>(null);
-    const [message, setMessage] = useState('');
-    const [uploadedImage, setUploadedImage] = useState<string>('');
-
-    const handleIdChange = (event: ChangeEvent<HTMLInputElement>) => {
-        setId(event.target.value);
-    };
-
-    const handleNameChange = (event: ChangeEvent<HTMLInputElement>) => {
-        setName(event.target.value);
-    };
-
-    const handleDescriptionChange = (event: ChangeEvent<HTMLInputElement>) => {
-        setDescription(event.target.value);
-    };
-
-    const handleTypeChange = (event: ChangeEvent<HTMLInputElement>) => {
-        setType(event.target.value);
-    };
-
-    const handleImageChange = (event: ChangeEvent<HTMLInputElement>) => {
-        if (event.target.files && event.target.files.length > 0) {
-            setImage(event.target.files[0]);
-            setUploadedImage(URL.createObjectURL(event.target.files[0]));
-        }
-    };
-
-    const handleSubmit = async (event: FormEvent) => {
-        event.preventDefault();
-
-        const formData = new FormData();
-        formData.append('id', id);
-        formData.append('name', name);
-        formData.append('description', description);
-        formData.append('type', type);
-        formData.append('image', image as File);
-
-        try {
-            const response = await fetch('/uploads', {
-                method: 'POST',
-                body: formData,
-            });
-
-            const result = await response.json();
-            if (response.ok) {
-                setMessage(result.message);
-                setId('');
-                setName('');
-                setDescription('');
-                setType('');
-                setImage(null);
-                setUploadedImage('');
-            } else {
-                throw new Error(result.error);
-            }
-        } catch (error) {
-            console.error(error);
-            setMessage('Error occurred during upload.');
-        }
-    };
+import { useState } from 'react';
+import storage from './firebase';
+function App() {
+    const [image, setImage] = useState<File | null>(null); // Use File | null for image state
+    const upload = () => {
+        if (image == null)
+            return;
+        storage.ref(`/images/${image.name}`).put(image)
+       
+    }
 
     return (
-        <div className="form-container">
-            <h1>Image Upload</h1>
-            <form onSubmit={handleSubmit} className="upload-form">
-                <div className="form-field">
-                    <label htmlFor="id">ID:</label>
-                    <input type="text" id="id" name="id" value={id} onChange={handleIdChange} required />
-                </div>
-
-                <div className="form-field">
-                    <label htmlFor="name">Name:</label>
-                    <input type="text" id="name" name="name" value={name} onChange={handleNameChange} required />
-                </div>
-
-                <div className="form-field">
-                    <label htmlFor="description">Description:</label>
-                    <input
-                        type="text"
-                        id="description"
-                        name="description"
-                        value={description}
-                        onChange={handleDescriptionChange}
-                        required
-                    />
-                </div>
-
-                <div className="form-field">
-                    <label htmlFor="type">Type:</label>
-                    <input type="text" id="type" name="type" value={type} onChange={handleTypeChange} required />
-                </div>
-
-                <div className="form-field">
-                    <label htmlFor="image">Image:</label>
-                    <input type="file" id="image" name="image" accept="image/*" onChange={handleImageChange} required />
-                </div>
-
-                {uploadedImage && <img src={uploadedImage} alt="Uploaded" className="uploaded-image" />}
-
-                <button type="submit" className="submit-button">Upload</button>
-            </form>
-
-            {message && <div>{message}</div>}
+        <div className="App">
+            <center>
+                <input type="file" onChange={(e:any) => { setImage(e.target.files[0]) }} />
+                <button onClick={upload}>Upload</button>
+            </center>
         </div>
     );
-};
+}
 
-export default ImageUpload;
+export default App;
--- src/Components/firebase.js
+++ src/Components/firebase.js
@@ -1,5 +1,4 @@
-import { initializeApp } from 'firebase/app';
-import { storage } from "firebase/storage";
+import firebase from 'firebase';
 const firebaseConfig = {
     apiKey: "AIzaSyBxJkm5Mo4ba1rqmfNatD36nnwtB17KM4c",
     authDomain: "rayuduart.firebaseapp.com",
@@ -8,7 +7,7 @@ const firebaseConfig = {
     messagingSenderId: "209584652865",
     appId: "1:209584652865:web:cddfeff526b8a7a05ffcc0",
     measurementId: "G-B2GN1FXJ1T"
-}
-
-const app = initializeApp(firebaseConfig);
-export const storage = getStorage(app);
+};
+firebase.initializeApp(firebaseConfig);
+var storage = firebase.storage();
+export default storage;
